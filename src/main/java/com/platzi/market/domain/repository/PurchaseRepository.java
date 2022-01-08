1e89e6331da35b0be1df81e307da81e0f399f7b0
xes28 <xescobarsmx2009@gmail.com>
2022-01-08 19:48:31 +0100

    Creación del repositorio de compras

--- src/main/java/com/platzi/market/domain/repository/PurchaseRepository.java
+++ src/main/java/com/platzi/market/domain/repository/PurchaseRepository.java
@@ -8,7 +8,7 @@ import java.util.Optional;
 public interface PurchaseRepository {
 
     List<Purchase> getAll();
-    Optional<List<Purchase>> getByClient(int clientId);
+    Optional<List<Purchase>> getByClient(String clientId);
     Purchase save (Purchase purchase);
 
 }
